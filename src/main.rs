349689d07d7d64ef29235c22dbc87ff48535a97b
André Medeiros <andre@setadesenvolvimentos.com.br>
2024-11-07 15:07:01 -0300

    Playground

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use std::{
 };
 use serde::{Serialize, Deserialize};
 use ::json_api::{
-    adapter::{UriGenerator, Adapter, Parameters},
-    resourceful::{Attributes, Relationships,RelatedCollection, RelatedData, RelatedRecord, Resourceful},
+    adapter::{Adapter, Context, DefaultUriGenerator, Parameters, UriGenerator},
+    resourceful::{
+        Attributes, Relationships, Resourceful,
+        related_data::{RelatedCollection, RelatedData, RelatedRecord}
+    },
     spec::identifier::Identifier,
      extract_filtered
 };
@@ -98,7 +101,7 @@ struct Post {
     id: String,
     title: String,
     content: String,
-    user_id: String,
+    author_id: String,
 }
 
 impl Identifiable for Post {
@@ -119,29 +122,27 @@ impl Resourceful for Record<User> {
         }
     }
 
-    fn attributes(&self, params: &Parameters) -> Option<Attributes> {
-        extract_filtered!(self.model, [name, age, active], params.fields_for(self.kind()))
+    fn attributes<G: UriGenerator>(&self, context: &Context<G>) -> Option<Attributes> {
+        extract_filtered!(self.model, [name, age, active], context.fields_for(self.kind()))
     }
 
-    fn relationships<G: UriGenerator>(&self, adapter: &mut Adapter<G>, params: &Parameters) -> Option<Relationships> {
-        if let Some(database) = self.database.upgrade() {
-            let posts = database.borrow().posts
-                .all()
-                .into_iter()
-                .filter_map(|post|
-                    if post.user_id == self.model.id {
-                        let record = Record { model: post, database: self.database.clone() };
-                        Some(adapter.make_resource(&record, params))
-                    } else {
-                        None
-                    }
-                );
-            Relationships::from([
-                ("posts".to_string(), RelatedData::Many(RelatedCollection::Loaded(posts.collect())))
-            ]).into()
-        } else {
-            None
-        }
+    fn relationships<G: UriGenerator>(&self, context: &mut Context<G>)
+        -> Option<Relationships>
+    {
+        let posts = self.database.upgrade()?.borrow()
+            .posts.all()
+            .into_iter()
+            .filter_map(|model|
+                if model.author_id == self.model.id {
+                    Some(Record { model, database: self.database.clone() })
+                } else {
+                    None
+                }
+            );
+
+        Relationships::from([
+            context.link_many("posts", posts),
+        ]).into()
     }
 }
 
@@ -154,27 +155,28 @@ impl Resourceful for Record<Post> {
         }
     }
 
-    fn attributes(&self, params: &Parameters) -> Option<Attributes> {
-        extract_filtered!(self.model, [title, content], params.fields_for(self.kind()))
+    fn attributes<G: UriGenerator>(&self, context: &Context<G>) -> Option<Attributes> {
+        extract_filtered!(self.model, [title, content], context.fields_for(self.kind()))
     }
 
-    fn relationships<G: UriGenerator>(&self, adapter: &mut Adapter<G>, params: &Parameters) -> Option<Relationships> {
-        if let Some(database) = self.database.upgrade() {
-            let user = database.borrow().users.find(&self.model.user_id).unwrap();
-            let record = Record { model: user, database: self.database.clone() };
-            Relationships::from([
-                ("user".to_string(), RelatedData::One(RelatedRecord::Unloaded(record.identifier())))
-            ]).into()
-        } else {
-            None
-        }
+    fn relationships<G: UriGenerator>(&self, context: &mut Context<G>)
+        -> Option<Relationships>
+    {
+        let author = self.database.upgrade()?.borrow()
+            .users
+            .find(&self.model.author_id)
+            .map(|model| Record { model, database: self.database.clone() });
+
+        Relationships::from([
+            context.link_one("author", author),
+            context.link_one("always_empty", Option::<Record<Post>>::None)
+        ]).into()
     }
 }
 
 fn main() {
     struct Generator;
     impl UriGenerator for Generator {}
-    let mut adapter = Adapter::new(Generator {});
 
     let database = Database::new(
         vec![
@@ -196,24 +198,26 @@ fn main() {
                 id: "5fc5ecc8-e286-4b04-9dd6-38306e7b5742".to_string(),
                 title: "This is a post".to_string(),
                 content: "This is the content of the post".to_string(),
-                user_id: "67e55044-10b1-426f-9247-bb680e5fe0c8".to_string(),
+                author_id: "67e55044-10b1-426f-9247-bb680e5fe0c8".to_string(),
             },
             Post {
                 id: "d22bcb69-e162-40d7-8e77-821e1be1e63e".to_string(),
                 title: "This is another post".to_string(),
                 content: "This is the content of the other post".to_string(),
-                user_id: "67e55044-10b1-426f-9247-bb680e5fe0c8".to_string(),
+                author_id: "67e55044-10b1-426f-9247-bb680e5fe0c8".to_string(),
             },
             Post {
                 id: "e2982a7a-9901-4ff9-a5ff-204ded2c21d5".to_string(),
                 title: "This is another post".to_string(),
                 content: "This is the content of the other post".to_string(),
-                user_id: "1cc61e1e-b862-4b3b-88a9-422ae3077145".to_string(),
+                author_id: "1cc61e1e-b862-4b3b-88a9-422ae3077145".to_string(),
             }
         ]
     );
 
-    let uri: Uri = "/users/67e55044-10b1-426f-9247-bb680e5fe0c8?fields[users]=name,active,non-existent-field".parse().unwrap();
+    let uri: Uri = "/users/67e55044-10b1-426f-9247-bb680e5fe0c8?fields[users]=name,active,non-existent-field&include=posts,always_empty&fields[posts]=title".parse().unwrap();
+    let generator = DefaultUriGenerator::default();
+    let mut adapter = Adapter::new(uri.clone(), generator);
     let user = database.borrow().users.find(&"67e55044-10b1-426f-9247-bb680e5fe0c8".into()).unwrap();
     let record = Record { model: user, database: Rc::downgrade(&database) };
     let document = adapter.make_resource_document(&record, &Parameters::from(uri));
