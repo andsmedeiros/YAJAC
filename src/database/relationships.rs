1f15c5ed4ebce71f108f3d1269ef504094f326b3
André Medeiros <andre@cronolink.com.br>
2025-10-10 11:32:41 -0300

    Removes the `Unloaded` value for `Relationship` enum (and also the `Default` impl)

--- src/database/relationships.rs
+++ src/database/relationships.rs
@@ -2,14 +2,9 @@ use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 pub enum Relationship {
-    Unloaded,
     BelongsTo(i32),
     HasOne(i32),
     HasMany(Vec<i32>),
-}
-
-impl Default for Relationship {
-    fn default() -> Self { Relationship::Unloaded }
 }
 
 pub type Relationships<'a> = HashMap<&'a str, Relationship>;
