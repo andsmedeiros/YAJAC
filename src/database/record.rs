5b43757e2c6e4ee01af6ed6fa87ce3491eb84ec1
André Medeiros <andre@cronolink.com.br>
2025-10-22 11:12:27 -0300

    Adds methods for retrieving the record type and identifier

--- src/database/record.rs
+++ src/database/record.rs
@@ -1,8 +1,10 @@
 use super::{
     attributes::Attributes,
+    error::Error,
     relationships::Relationships,
     schema::TableSchema,
 };
+use crate::json_api::identifier::Identifier;
 
 #[derive(Debug, Clone)]
 pub struct Record<'a> {
@@ -18,5 +20,27 @@ impl<'a> Record<'a> {
             attributes: Attributes::new(),
             relationships: Relationships::new()
         }
+    }
+
+    pub fn kind(&self) -> &'a str {
+        self.schema.name
+    }
+
+    pub fn identifier(&self) -> Result<Identifier, Error> {
+        // TODO: Consider implementing other id types specified by the schema
+        let identifier = match self.attributes.get("id") {
+            Some(id) => Identifier::Existing {
+                kind: self.kind().to_string(),
+                id: id.as_i64()?.to_string()
+            },
+            None => Identifier::New {
+                kind: self.kind().to_string(),
+                lid: self.attributes.get("lid")
+                    .map(|lid| -> Result<_, Error> { Ok(lid.as_i64()?.to_string()) })
+                    .transpose()?
+            }
+        };
+
+        Ok(identifier)
     }
 }
